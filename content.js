2aa8d2f0d8b91eb518c523d45d1a0c7a8b74b7cb
Colin R <csrichards.90@gmail.com>
2019-09-22 22:04:18 -0500

    Select youtube video player

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
 // Script for DOM manipulation
 // Listen for the message sent in background.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(sender);
+    
+    let video = document.querySelector('.html5-main-video');
+    //console.log(video.properties);
+
+    if (!video.muted) {
+        video.muted = true;
+        console.log(video.muted);
+        video.mute();
+    }
+
     if (request.message === 'loaded_channel_page') {
         console.log('Loaded channel');
     }
 })
-console.log('content.js working');
+// console.log('content.js working');
