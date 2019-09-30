1b4229f6186e7086a5bf228d480ab5ecf6b2b7c1
Colin R <csrichards.90@gmail.com>
2019-09-29 22:29:13 -0500

    Implement mute video on channel page load

--- content.js
+++ content.js
@@ -1,18 +1,20 @@
 // Script for DOM manipulation
 // Listen for the message sent in background.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    
-    let video = document.querySelector('.html5-main-video');
-    //console.log(video.properties);
-
-    if (!video.muted) {
-        video.muted = true;
-        console.log(video.muted);
-        video.mute();
-    }
 
     if (request.message === 'loaded_channel_page') {
-        console.log('Loaded channel');
+
+        // console.log('Loaded channel');
+        
+        let video = document.querySelector('.html5-main-video');
+        let newVolume = video.volume;
+        // console.log(newVolume);
+
+        video.addEventListener('loadeddata', (event) => {
+            // console.log('fired');
+            newVolume = 0;
+            video.volume = newVolume;
+        })
     }
+
 })
-// console.log('content.js working');
