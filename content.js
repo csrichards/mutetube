0242b22ee9c781eaa7a2340b73e1e1a4bbe52e56
Colin R <csrichards.90@gmail.com>
2019-10-13 22:39:40 -0500

    Initial branch commit

--- content.js
+++ content.js
@@ -1,53 +1,65 @@
 // Script for DOM manipulation
 // Listen for the message sent in background.js
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
-    // if (request.message === 'loaded_channel_page' && document.readyState != 'loading') {
-    //     console.log(request.message);
-    //     let newVolume = 0;
-    //     console.log('newVolume: ', newVolume);
-
-    //     document.addEventListener('yt-navigate-finish', () => {
-    //         console.log("navigation finished");
-    //         setTimeout(mute, 2000);
-    //     });
-    
-    //     function mute() {
-    //         let video = document.querySelector('.html5-main-video');
-    //         console.log("video test", video);
-    //         console.log(video.volume);
-    //         video.volume = newVolume;
-    //         console.log(video.volume);
-    //     }
-    // }
-
     if (request.message === 'loaded_channel_page') {
-        // let s = document.createElement('script');
-        // s.src = chrome.runtime.getURL('muteVideo.js');
-        // s.onload = function() {
-        //     this.remove();
-        // };
-        // (document.head || document.documentElement).appendChild(s);
+        let s = document.createElement('script');
+        s.src = chrome.runtime.getURL('muteVideo.js');
+        s.onload = function() {
+            this.remove();
+        };
+        console.log(s);
+        document.documentElement.appendChild(s);
+
+        /*
+
         console.log('loaded channel page');
-        //inPage(setVolume, 0);
-        setTimeout(inPage, 2000, setVolume, 0);
+        // inPage(setVolume, 0);
+        setTimeout(inPage, 5000, setVolume, 0);
         // document.addEventListener('yt-navigate-finish', () => {
         //     console.log("navigation finished");
         //     setTimeout(inPage, 2000, setVolume, 0);
         // });
+
+        // function setVolume(vol) {
+        //     console.log('set volume');
+        //     let videoPlayer = document.getElementById('movie_player');
+        //     // let video = videoPlayer.querySelector('video');
+        //     //let video = document.querySelector('video');
+        //     console.log(videoPlayer);
+        //     //console.log(video);
+        //     console.log('volume1 ', videoPlayer.getVolume());
+        //     //console.log(video.volume);
+        //     videoPlayer.setVolume(vol);
+        //     //video.volume = vol;
+        //     //console.log(video.volume);
+        //     console.log('volume2 ', videoPlayer.getVolume());
+        // }
+
         function setVolume(vol) {
-            console.log('set volume');
-            let videoPlayer = document.getElementById('movie_player');
-            let video = videoPlayer.querySelector('video');
-            //let video = document.querySelector('video');
-            console.log(videoPlayer);
-            //console.log(video);
-            console.log('volume1 ', videoPlayer.getVolume());
-            //console.log(video.volume);
-            videoPlayer.setVolume(vol);
-            //video.volume = vol;
-            //console.log(video.volume);
-            console.log('volume2 ', videoPlayer.getVolume());
+            let tag = document.createElement('script');
+            tag.id = 'iframe-demo';
+            tag.src = 'https://www.youtube.com/iframe_api';
+            let firstScriptTag = document.getElementsByTagName('script')[0];
+            //Insert tag before the first script tag
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            console.log('tag ', tag);
+            let player;
+            function onYouTubeIframeAPIReady() {
+                console.log('on youtube iframe api ready');
+                player = new YT.Player('existing-iframe-example', {
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                });
+            }
+
+            function onPlayerReady(event) {
+                console.log('on player ready');
+                console.log(event.target);
+            }
         }
 
         function inPage(fn, ...args) {
@@ -61,5 +73,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             console.log(document.documentElement);
             s.remove();
         }
+        */
     }
 });
--- muteVideo.js
+++ muteVideo.js
@@ -1,7 +1,47 @@
-console.log('in page');
+// console.log('in page');
 
-inPage(setVolume, 0);
+// inPage(setVolume, 0);
 
-function setVolume(vol) {
-    document.getElementById('movie_player').setVolume(vol);
+// function setVolume(vol) {
+//     document.getElementById('movie_player').setVolume(vol);
+// }
+let wrapper = document.querySelector('iframe');
+console.log(wrapper);
+
+let tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+    console.log('on youtube iframe api ready');
+    player = new YT.Player(wrapper, {
+        events: {
+            'onReady': function(event) {
+                onPlayerReady(event.target)
+            }
+        }
+    });
+    console.log(player);
 }
+
+function onPlayerReady(event) {
+    console.log('player is ready');
+    console.log(event);
+}
+
+let vol = 0;
+
+//let videoPlayer = document.getElementById('movie_player');
+// let video = videoPlayer.querySelector('video');
+//let video = document.querySelector('video');
+//console.log(videoPlayer);
+//console.log(video);
+//console.log('volume1 ', videoPlayer.getVolume());
+//console.log(video.volume);
+//videoPlayer.setVolume(vol);
+//video.volume = vol;
+//console.log(video.volume);
+//console.log('volume2 ', videoPlayer.getVolume());
