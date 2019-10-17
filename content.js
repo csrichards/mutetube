af049d074b486af99af04a70277b2e565eca28b5
Colin R <csrichards.90@gmail.com>
2019-10-16 23:11:24 -0500

    works again

--- content.js
+++ content.js
@@ -2,25 +2,6 @@
 // Listen for the message sent in background.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
-    // if (request.message === 'loaded_channel_page' && document.readyState != 'loading') {
-    //     console.log(request.message);
-    //     let newVolume = 0;
-    //     console.log('newVolume: ', newVolume);
-
-    //     document.addEventListener('yt-navigate-finish', () => {
-    //         console.log("navigation finished");
-    //         setTimeout(mute, 2000);
-    //     });
-    
-    //     function mute() {
-    //         let video = document.querySelector('.html5-main-video');
-    //         console.log("video test", video);
-    //         console.log(video.volume);
-    //         video.volume = newVolume;
-    //         console.log(video.volume);
-    //     }
-    // }
-
     if (request.message === 'loaded_channel_page') {
         // let s = document.createElement('script');
         // s.src = chrome.runtime.getURL('muteVideo.js');
@@ -30,24 +11,18 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         // (document.head || document.documentElement).appendChild(s);
         console.log('loaded channel page');
         //inPage(setVolume, 0);
-        setTimeout(inPage, 2000, setVolume, 0);
-        // document.addEventListener('yt-navigate-finish', () => {
-        //     console.log("navigation finished");
-        //     setTimeout(inPage, 2000, setVolume, 0);
-        // });
-        function setVolume(vol) {
+        setTimeout(inPage, 2000, newVolume, 0);
+
+        function newVolume(vol) {
             console.log('set volume');
+            // let videoPlayer = document.getElementById('movie_player');
             let videoPlayer = document.getElementById('movie_player');
-            let video = videoPlayer.querySelector('video');
-            //let video = document.querySelector('video');
-            console.log(videoPlayer);
-            //console.log(video);
-            console.log('volume1 ', videoPlayer.getVolume());
-            //console.log(video.volume);
-            videoPlayer.setVolume(vol);
-            //video.volume = vol;
-            //console.log(video.volume);
-            console.log('volume2 ', videoPlayer.getVolume());
+            let muteButton = document.querySelector('.ytp-mute-button');
+            let muteButtonDiv = document.querySelector('.ytp-volume-panel');
+            let videos = document.querySelectorAll('video')
+            console.log(videos[0]);
+
+            videos[0].volume = vol;
         }
 
         function inPage(fn, ...args) {
