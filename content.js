c8c10109586fba7b606331e28d80f818cb5a8e23
[Richards Colin] <[csrichards.90@gmail.com]>
2019-11-18 19:02:23 -0600

    Stop calls to newVolume after x tries

--- content.js
+++ content.js
@@ -3,30 +3,49 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.message === 'loaded_channel_page') {
-        // let s = document.createElement('script');
-        // s.src = chrome.runtime.getURL('muteVideo.js');
-        // s.onload = function() {
-        //     this.remove();
-        // };
-        // (document.head || document.documentElement).appendChild(s);
-        console.log('master branch: loaded channel page');
+        console.log('continual_check branch: loaded channel page');
         //inPage(setVolume, 0);
-        setTimeout(inPage, 4000, newVolume, 0);
+        // let tries = 0;
+        // console.log('tries: ', tries);
 
-        function newVolume(vol) {
+        setTimeout(inPage, 1000, newVolume, 0, 0);
+
+        function newVolume(vol, tries) {
             console.log('set volume');
+            // console.log(tries);
             // let videoPlayer = document.getElementById('movie_player');
+            // console.log(videos);
             let videoPlayer = document.getElementById('movie_player');
             let muteButton = document.querySelector('.ytp-mute-button');
             let muteButtonDiv = document.querySelector('.ytp-volume-panel');
-            let videos = document.querySelectorAll('video')
-            console.log(videos);
-
-            videos.forEach((vid) => {
-                console.log(vid);
-                vid.volume = vol;
-           })
+            let videos = document.querySelectorAll('video');
+            // console.log(videos);
+            console.log(tries);
+            vidSrc = [...videos].filter((vid) => vid.hasAttribute('src'));
+            console.log('vidSrc', vidSrc);
+            if (vidSrc.length > 0 && tries <= 5) {
+                console.log('muting videos...');
+                // vidSrc.forEach((vid) => {
+                //     console.log('muted video');
+                //     vid.volume = vol;
+                // })
+                videos.forEach((vid) => {
+                        console.log('muted video ', vid);
+                        vid.volume = vol;
+                })
+                s.remove();
+            }
+            else if (tries > 5) {
+                s.remove();
+            }
+            else {
+                // If video src's are not loaded, wait X seconds and call newVolume again
+                console.log('no videos ', vidSrc);
+                tries += 1;
+                setTimeout(newVolume, 1000, 0, tries);
+            }
         }
+        
 
         function inPage(fn, ...args) {
             console.log('in page');
@@ -35,8 +54,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             s.textContent = `(${fn})(${JSON.stringify(args).slice(1, -1)})`;
             s.type = 'text/javascript';
             document.documentElement.appendChild(s);
-            console.log(document.documentElement);
-            s.remove();
+            //console.log(document.documentElement);
+            //s.remove();
         }
     }
 });
